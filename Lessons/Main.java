bd261825590c326dfd5883a766b8feaa2d6a4040
RomanKryvolapov <37460379+RomanKryvolapov@users.noreply.github.com>
2018-03-26 02:11:38 +0300

    Update Main.java

--- Lessons/Main.java
+++ Lessons/Main.java
@@ -5,17 +5,16 @@ package com.company;
 // Вывод:
 //
 //Проверка >> Длинна а = 1000
-//Метод a.concat >> время выполнения    1 866 661 наносекунд
+//Метод a.concat >> время выполнения    1 398 751 наносекунд
 //
 //Проверка >> Длинна а = 1000
-//Метод a = a + b >> время выполнения   38 209 199 наносекунд
+//Метод a = a + b >> время выполнения   48 299 503 наносекунд
 //
 //Проверка >> Длинна а = 1000
-//Метод StringBuilder >> время выполнения    2 099 119 наносекунд
+//Метод StringBuilder >> время выполнения    2 069 626 наносекунд
 //
 //Проверка >> Длинна а = 1000
-//Метод StringBuffer >> время выполнения    3 159 427 наносекунд
-
+//Метод StringBuffer >> время выполнения    3 559 796 наносекунд
 
 public class Main {
 
@@ -31,32 +30,36 @@ public class Main {
         start = System.nanoTime();
         String a = "";
         String b = "1";
-        for (int i = 0; i < 1000; i++) {
+
             switch (method) {
                 case "a.concat":
+                    for (int i = 0; i < 1000; i++) {
                     a = a.concat(b);
+                    }
                     break;
                 case "a = a + b":
+                    for (int i = 0; i < 1000; i++) {
                     a = a + b;
+                    }
                     break;
                 case "StringBuilder":
+                    for (int i = 0; i < 1000; i++) {
                     StringBuilder a1 = new StringBuilder(a);
                     a1.append(b);
                     a = a1.toString();
+                    }
                     break;
                 case "StringBuffer":
+                    for (int i = 0; i < 1000; i++) {
                     StringBuffer a2 = new StringBuffer(a);
                     a2.append(b);
                     a = a2.toString();
+                    }
                     break;
-            }
+
         }
         stop = System.nanoTime();
         System.out.println("\nПроверка >> Длинна а = " + a.length());
         return stop - start;
     }
 }
-
-
-
-
