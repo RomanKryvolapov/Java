7528adb427e81bf4441e6863b59ef4001db7e113
Roman Kryvolapov <roman.kryvolapov@gmail.com>
2018-04-12 00:29:59 +0300

    new

--- Android String Calculator/java/com/stringcalculator/my/stringcalculator/MainActivity.java
+++ Android String Calculator/java/com/stringcalculator/my/stringcalculator/MainActivity.java
@@ -6,15 +6,19 @@ package com.stringcalculator.my.stringcalculator;
 // Есть история
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import java.math.BigDecimal;
@@ -127,6 +131,17 @@ public class MainActivity extends AppCompatActivity {
         button_ravno_listiner(buttonRavno);
         button_history_listiner(buttonHistory);
 
+        setStatusBarColor(R.color.colorPrimary);
+
+    }
+
+    public void setStatusBarColor(int color) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, color));
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
