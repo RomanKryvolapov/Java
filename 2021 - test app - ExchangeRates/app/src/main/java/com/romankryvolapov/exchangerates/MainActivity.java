aab812e4b08f5256c10db123b855977eac73f0b8
Roman Kryvolapov <roman.kryvolapov@gmail.com>
2021-02-10 00:53:06 +0200

    update

--- 2021 - test app - ExchangeRates/app/src/main/java/com/romankryvolapov/exchangerates/MainActivity.java
+++ 2021 - test app - ExchangeRates/app/src/main/java/com/romankryvolapov/exchangerates/MainActivity.java
@@ -1,15 +1,21 @@
 package com.romankryvolapov.exchangerates;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -30,16 +36,19 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private TableLayout PBcourse;
     private TableLayout NBUcourse;
     private boolean rowCounter = false;
-    private String TAG = "MainActivity";
+    private final String TAG = "MainActivity";
     private int textSize = 18;
-    private String currentDateAndTime;
+    private String dateAndTimeString;
     private TextView dateAndTime;
+    private TextView dateAndTime2;
+    private SimpleDateFormat simpleDateFormat;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +56,38 @@ public class MainActivity extends AppCompatActivity {
         PBcourse = findViewById(R.id.PBcourse);
         NBUcourse = findViewById(R.id.NBUcourse);
         dateAndTime = findViewById(R.id.dateAndTime);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
-        currentDateAndTime = sdf.format(new Date());
-        getCourseJson(currentDateAndTime);
-        dateAndTime.setText(currentDateAndTime);
+        dateAndTime2 = findViewById(R.id.dateAndTime2);
+        ImageButton dateAndTimeButton = findViewById(R.id.dateAndTimeButton);
+        ImageButton dateAndTimeButton2 = findViewById(R.id.dateAndTimeButton2);
+        simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        dateAndTimeString = simpleDateFormat.format(new Date());
+        dateAndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate();
+            }
+        });
+        dateAndTime2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate();
+            }
+        });
+        dateAndTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate();
+            }
+        });
+        dateAndTimeButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate();
+            }
+        });
+        getCourseJson(dateAndTimeString);
+//        getCourseJson("09.02.2021");
+
     }
 
     private TableRow setPBcourse(String currency, String purchaseRate, String saleRate) {
@@ -58,34 +95,9 @@ public class MainActivity extends AppCompatActivity {
         TableRow tableRow = new TableRow(this);
         tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         tableRow.setGravity(Gravity.CENTER);
-
-        TextView textViewCurrency = new TextView(this);
-        textViewCurrency.setText(currency);
-        textViewCurrency.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewCurrency.setGravity(Gravity.CENTER);
-        textViewCurrency.setTextColor(getResources().getColor(R.color.text_2));
-        textViewCurrency.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
-        textViewCurrency.setTypeface(textViewCurrency.getTypeface(), Typeface.BOLD);
-        tableRow.addView(textViewCurrency);
-
-        TextView textViewPurchaseRate = new TextView(this);
-        textViewPurchaseRate.setText(purchaseRate);
-        textViewPurchaseRate.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewPurchaseRate.setGravity(Gravity.CENTER);
-        textViewPurchaseRate.setTextColor(getResources().getColor(R.color.text_2));
-        textViewPurchaseRate.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
-        textViewPurchaseRate.setTypeface(textViewPurchaseRate.getTypeface(), Typeface.BOLD);
-        tableRow.addView(textViewPurchaseRate);
-
-        TextView textViewSaleRate = new TextView(this);
-        textViewSaleRate.setText(saleRate);
-        textViewSaleRate.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewSaleRate.setGravity(Gravity.CENTER);
-        textViewSaleRate.setTextColor(getResources().getColor(R.color.text_2));
-        textViewSaleRate.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
-        textViewSaleRate.setTypeface(textViewSaleRate.getTypeface(), Typeface.BOLD);
-        tableRow.addView(textViewSaleRate);
-
+        tableRow.addView(textViewGenerator(currency, Gravity.CENTER, 0, 0));
+        tableRow.addView(textViewGenerator(purchaseRate + " UAH", Gravity.CENTER, 0, 0));
+        tableRow.addView(textViewGenerator(saleRate + " UAH", Gravity.CENTER, 0, 0));
         return tableRow;
     }
 
@@ -95,36 +107,20 @@ public class MainActivity extends AppCompatActivity {
         tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         tableRow.setGravity(Gravity.CENTER);
 
-        if(rowCounter){
+        if (rowCounter) {
             rowCounter = false;
             tableRow.setBackgroundColor(getResources().getColor(R.color.color_2));
         } else {
             rowCounter = true;
         }
 
-        TextView textViewPurchaseRateNB = new TextView(this);
-        textViewPurchaseRateNB.setPadding(100, 0, 0, 0);
-        textViewPurchaseRateNB.setText(currency);
-        textViewPurchaseRateNB.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewPurchaseRateNB.setGravity(Gravity.START);
-        textViewPurchaseRateNB.setTextColor(getResources().getColor(R.color.text_2));
-        textViewPurchaseRateNB.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
-        textViewPurchaseRateNB.setTypeface(textViewPurchaseRateNB.getTypeface(), Typeface.BOLD);
-        tableRow.addView(textViewPurchaseRateNB);
-
-        TextView textViewSaleRateNB = new TextView(this);
-        textViewSaleRateNB.setPadding(0, 0, 100, 0);
-        textViewSaleRateNB.setText(purchaseRateNB);
-        textViewSaleRateNB.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewSaleRateNB.setGravity(Gravity.END);
-        textViewSaleRateNB.setTextColor(getResources().getColor(R.color.text_2));
-        textViewSaleRateNB.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
-        textViewSaleRateNB.setTypeface(textViewSaleRateNB.getTypeface(), Typeface.BOLD);
-        tableRow.addView(textViewSaleRateNB);
+        tableRow.addView(textViewGenerator(currency, Gravity.START, 100, 0));
+        tableRow.addView(textViewGenerator(purchaseRateNB + " UAH", Gravity.END, 0, 100));
 
         return tableRow;
     }
 
+    @SuppressLint("DefaultLocale")
     private void parseCourse(String dataJson) {
         try {
             JSONObject jsonObject = new JSONObject(dataJson);
@@ -132,14 +128,19 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObjectValue = jsonArray.getJSONObject(i);
                 String currency = jsonObjectValue.optString("currency", null);
-                String purchaseRate = jsonObjectValue.optString("purchaseRate", null);
-                String saleRate = jsonObjectValue.optString("saleRate", null);
-                String purchaseRateNB = jsonObjectValue.optString("purchaseRateNB", null);
-                String saleRateNB = jsonObjectValue.optString("saleRateNB", null);
-                if (currency != null && currency.length()>0 && purchaseRate != null && saleRate != null)
-                    PBcourse.addView(setPBcourse(currency, purchaseRate, saleRate));
-                if (currency != null && currency.length()>0 && purchaseRateNB != null && saleRateNB != null && !currency.equals("UAH"))
-                    NBUcourse.addView(setNBcourse(currency, purchaseRateNB + " UAH"));
+                double purchaseRate = jsonObjectValue.optDouble("purchaseRate", 0d);
+                double saleRate = jsonObjectValue.optDouble("saleRate", 0d);
+                double purchaseRateNB = jsonObjectValue.optDouble("purchaseRateNB", 0d);
+//                double saleRateNB = jsonObjectValue.optDouble("saleRateNB", 0d);
+                String purchaseRateString = String.format("%.2f", purchaseRate);
+                String saleRateString = String.format("%.2f", saleRate);
+                String purchaseRateNBString = String.format("%.2f", purchaseRateNB);
+                if (currency != null && currency.length() > 0 && !purchaseRateString.equals("0.00") && !saleRateString.equals("0.00")) {
+                    PBcourse.addView(setPBcourse(currency, purchaseRateString, saleRateString));
+                }
+                if (currency != null && currency.length() > 0 && !purchaseRateNBString.equals("0.00") && !currency.equals("UAH")) {
+                    NBUcourse.addView(setNBcourse(currency, purchaseRateNBString));
+                }
             }
         } catch (Exception e) {
             Log.e(TAG, "parsePBcourse Exception " + e);
@@ -147,14 +148,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private void getCourseJson(String date) {
         new ObservableCreate<String>(emitter -> {
             Request.Builder builder = new Request.Builder();
             builder.url("https://api.privatbank.ua/p24api/exchange_rates?json&date=" + date);
             Request request = builder.build();
             Response response = buildHttpClient().newCall(request).execute();
-            emitter.onNext(response.body().string());
+            if (response.code() >= 200 && response.code() < 300) {
+                emitter.onNext(response.body().string());
+            } else {
+                throw new Exception();
+            }
             emitter.onComplete();
         })
                 .subscribeOn(Schedulers.io())
@@ -162,6 +166,8 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(new Observer<String>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
+                        dateAndTime.setText(dateAndTimeString);
+                        dateAndTime2.setText(dateAndTimeString);
                     }
 
                     @Override
@@ -171,6 +177,9 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(@NonNull Throwable e) {
+                        Log.e(TAG, "getCourseJson Exception" + e);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Нет интернет соединения", Toast.LENGTH_SHORT);
+                        toast.show();
                     }
 
                     @Override
@@ -181,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private OkHttpClient buildHttpClient() {
+    private static OkHttpClient buildHttpClient() {
         return new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(10, TimeUnit.SECONDS)
@@ -189,5 +198,38 @@ public class MainActivity extends AppCompatActivity {
                 .build();
     }
 
+    public TextView textViewGenerator(String text, int gravity, int start, int end) {
+        TextView textView = new TextView(this);
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+        layoutParams.setMarginStart(start);
+        layoutParams.setMarginEnd(end);
+        textView.setText(text);
+        textView.setLayoutParams(layoutParams);
+        textView.setGravity(gravity);
+        textView.setTextColor(getResources().getColor(R.color.text_2));
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
+        textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+        return textView;
+    }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        Date date = calendar.getTime();
+        PBcourse.removeAllViews();
+        NBUcourse.removeAllViews();
+        dateAndTimeString = simpleDateFormat.format(date);
+        getCourseJson(dateAndTimeString);
+    }
+
+    public void setDate() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        new DatePickerDialog(MainActivity.this, this, year, month, day).show();
+    }
 }
